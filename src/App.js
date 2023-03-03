204829266c61d2b6076f7311ec5c268e5f1f8029
Yzy <2154597198@qq.com>
2023-03-03 20:52:22 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='App'>
+      <Menu />
+      <MainWindow />
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div id='Menu'>
+      <table>
+        <th><a href='index.html'>首页</a></th>
+        <th><a href='profession.html'>专业</a></th>
+        <th><a href='lessons.html'>课程</a></th>
+        <th><a href='srtp.html'>SRTP</a></th>
+        <th><a href='other.html'>其他</a></th>
+        <th><a href='submit.html'>投稿</a></th>
+        <th><a href='about.html'>关于</a></th>
+      </table>
+    </div>
+  )
+}
+
+function MainWindow() {
+  return (
+    <div id='mainWindow'>
+      <div class='list'>
+        <dl>
+          <caption><b>最新更新</b></caption>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+        </dl>
+      </div>
+      <div class='list'>
+        <dl>
+          <caption><b>点赞最多</b></caption>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+          <dt class='Update'><a href='https://www.icourse163.org/course/ZJU-1206573810'>机器学习-浙江大学-胡浩基</a></dt>
+        </dl>
+      </div>
     </div>
   );
 }
